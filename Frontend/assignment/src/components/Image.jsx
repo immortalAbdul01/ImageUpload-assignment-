15685f00c680f2cc07994f1623d5368b86e7e5ec
Immortal <ak2061435@gmail.com>
2023-02-28 21:45:24 +0530

    update Image.js

--- Frontend/assignment/src/components/Image.jsx
+++ Frontend/assignment/src/components/Image.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function ImageGallery() {
+function Image() {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
@@ -20,4 +20,4 @@ function ImageGallery() {
     );
 }
 
-export default ImageGallery;
+export default Image;
