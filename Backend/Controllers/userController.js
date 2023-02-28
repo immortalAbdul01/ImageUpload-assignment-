3e4a414a9af9326f9ea17c64727cbbe9ccf35d22
Immortal <ak2061435@gmail.com>
2023-02-28 18:15:45 +0530

    Login and sign up functionality in backend

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,11 +1,97 @@
 const User = require('./../Models/userModle')
-exports.SignIn = async (req, res) => {
-    const user = await User.create(req.body)
-    res.status(201).json({
-        msg: 'sucess',
-        user
+
+const jwt = require('jsonwebtoken')
+
+
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRESIN
     })
 
+}
+const cookieOption = {
+    Expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRESIN * 24 * 60 * 60 * 1000),
 
+
+    HttpOnly: true
+}
+if (process.env.NODE_ENV === 'production') {
+    cookieOption.Secure = true
+}
+
+
+
+const createToken = (user, statusCode, res) => {
+    const token = signToken(user._id)
+
+    res.cookie('jwt', token, cookieOption)
+    res.status(statusCode).json({
+        mssg: 'token send sucessfully',
+        token
+    })
+}
+
+exports.signIn = async (req, res) => {
+    try {
+
+
+        const user = await User.create(req.body)
+
+        // createToken(user, 201, res)
+        res.status(201).json({
+            mssg: 'sucess',
+
+            data: {
+                user
+            }
+
+        })
+    } catch (err) {
+        res.status(404).json({
+            mssg: 'failed',
+            mssg: err.message
+        })
+    }
 
 }
+exports.login = async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email }).select('+createPassword')
+    if (!await user.correctPassword(password, user.createPassword)) {
+        res.status(404).json({
+            mssg: 'something went wrong'
+        })
+
+    }
+
+    const token = signToken(user._id)
+    res.status(200).json({
+        mssg: 'logged in sucessfully',
+        token
+    })
+
+}
+
+exports.getAlluser = async (req, res) => {
+    const users = await User.find()
+    res.status(200).json({
+        mssg: 'sucess',
+        data: users
+    })
+}
+exports.singleUser = async (req, res) => {
+    try {
+
+        const user = await User.findById(req.params.id)
+        res.status(200).json({
+            mssg: 'sucess',
+            user
+        })
+    } catch (err) {
+        res.status(404).json({
+            mssg: err.message
+        })
+    }
+}
+
--- Backend/Models/userModle.js
+++ Backend/Models/userModle.js
@@ -1,20 +1,63 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const crypto = require('crypto')
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: true,
+            required: true
         },
         email: {
             type: String,
-            required: true,
-
+            unique: true,
+            lowercase: true,
+            validate: [validator.isEmail]
         },
-        password: {
+
+
+        photo: {
+            type: String
+        },
+        createPassword: {
             type: String,
-            required: true
-        }
+
+            required: true,
+            minlength: 6,
+        },
+        passwordConfirm: {
+            type: String,
+            validate: {
+                validator: function (el) {
+                    return el === this.createPassword
+                },
+                message: 'the passwords are not same'
+            }
+        },
+        passwordChangeAt: Date,
+        passwordRestToken: String,
+        passwordResetExpires: Date,
+        updatePassword: String,
     }
+
+
 )
-const User = mongoose.model('User', userSchema)
-module.exports = User;
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('createPassword')) return next()
+    this.createPassword = await bcrypt.hash(this.createPassword, 12)
+    this.passwordConfirm = undefined
+    next()
+})
+userSchema.methods.correctPassword = (async function (candidatePassword, password) {
+    return await bcrypt.compare(candidatePassword, password)
+})
+userSchema.methods.createPasswordRestToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+
+    this.passwordRestToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+    console.log({ resetToken }, this.passwordRestToken);
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+    return resetToken
+}
+const User = mongoose.model('user', userSchema)
+module.exports = User
