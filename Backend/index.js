0eed464e327e03f3ee9ee15f772d422c3570210a
Immortal <ak2061435@gmail.com>
2023-02-28 18:10:24 +0530

    backend and frontend added

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,60 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
-dotenv.config()
+const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`server has been  started on ${process.env.PORT}`);
-})
+// Set up middleware
+app.use(express.json());
+app.use(cors());
+
+// Set up MongoDB connection
+
 mongoose.connect(process.env.LINK, () => {
     console.log('connected with mongoose');
 })
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Set up file upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        cb(null, uuidv4() + ext);
+    },
+});
+const upload = multer({ storage: storage });
+
+// Set up routes
+app.post('/upload', upload.single('file'), (req, res) => {
+    const file = req.file;
+    const newImage = { name: file.filename };
+    Image.create(newImage, (err, image) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error uploading image');
+        }
+        return res.status(200).send(image);
+    });
+});
+
+// Set up database schema
+const imageSchema = new mongoose.Schema({
+    name: String,
+});
+const Image = mongoose.model('Image', imageSchema);
+
+// Start the server
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
--- Frontend/assignment/src/App.js
+++ Frontend/assignment/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+import './App.css'
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (files) => {
+    setSelectedFile(URL.createObjectURL(files[0]));
+    const formData = new FormData();
+    formData.append('file', files[0]);
+    axios.post('http://localhost:5000/upload', formData).then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select an image to display</h1>
+      <Dropzone onDrop={handleFileSelect}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop an image here, or click toselect a file</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+      {selectedFile && <img className='img' src={selectedFile} alt="Selected file" />}
     </div>
   );
 }
